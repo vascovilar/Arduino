df10ec9e678b26c90628f558b4c869a393ca2a1c
vasco <vasco.vilar@gmail.com>
2022-10-24 17:14:26 +0200

    touch screen

--- libraries/VCom/VComScreen.cpp
+++ libraries/VCom/VComScreen.cpp
@@ -1,18 +1,145 @@
 #include "VComScreen.h"
 
-void VComScreen::begin() 
+void VComScreen::begin(String* menu, int size) 
 {
+  _menu = menu;
+  _size = size;
+  
   _tft.init();
   _tft.setRotation(0);
-  clear();
+
+  next(0);
 }
 
-void VComScreen::menu() 
-{
-  _tft.setTextColor(TFT_WHITE);
+void VComScreen::next(int i) 
+{  
+  String page = _matchMenu(_word);
 
-  _tft.fillRect(0, 74, 240, 24, TFT_BLUE);
-  _tft.drawCentreString("air", 120, 50, 4);
-  _tft.drawCentreString("light", 120, 74, 4);
-  _tft.drawCentreString("gps", 120, 98, 4);
+  _position += i;
+  if (_position < 0) {
+    _position = _countData(page)-1;
+  }
+  if (_position > (_countData(page)-1)) {
+    _position = 0;
+  }
+  clear();
+  _iterateData(page, _position);
+}
+
+void VComScreen::click(std::function<bool(String)> callback)
+{
+  String line = _getData(_matchMenu(_word), _position);
+  
+  if (line == SCREEN_BACK_WORD) {
+    _word = "";
+    _position = 0;
+    next(0);
+  } else if (!callback(line)) {
+    _word = line;
+    _position = 0;
+    next(0);
+  }
+}
+
+String VComScreen::_enumMenu()
+{
+  String out = "";
+
+  for (int i=0; i<_size; i++) 
+  { 
+    String words = _menu[i];
+    int index = 0;
+ 
+    while(index != -1) {
+      index = words.indexOf(" > ");
+      if (index != -1) {
+        out = _addData(out, words.substring(0, index));
+        words = words.substring(index+3, words.length());
+      } else {
+        out = _addData(out, words);
+      }
+    }
+  }
+
+  return out;
+}
+
+String VComScreen::_matchMenu(String command)
+{
+  if (command == "") {
+    return _enumMenu();
+  }
+  
+  String out = SCREEN_BACK_WORD;
+  
+  for (int i=0; i<_size; i++) 
+  { 
+    String words = _menu[i];
+    if (words.indexOf(command) != -1) {
+      out = _addData(out, words);
+    }
+  }
+
+  return out;
+}
+
+String VComScreen::_addData(String data, String command) 
+{
+  if (data.indexOf(command) == -1) {
+    if (data != "") {
+      data += "\n";
+    }
+    data += command;
+  }
+
+  return data;
+}
+
+int VComScreen::_countData(String data)
+{
+  int length = 1;
+  
+  for (int i = 0; i<data.length(); i++) {
+    if (String(data.charAt(i)) == "\n") {
+      length++;
+    }
+  }
+
+  return length;
+}
+
+String VComScreen::_getData(String data, int position)
+{
+  int rank = 0;
+  int index = 0;
+
+  while(index != -1) {
+    index = data.indexOf("\n");
+    if (rank == position) {
+      if (index != -1) {
+        return data.substring(0, index);
+      } else {
+        return data;
+      }      
+    }
+    data = data.substring(index+1, data.length());
+    rank++;
+  }
+}
+
+void VComScreen::_iterateData(String data, int position)
+{
+  int rank = 0;
+  int index = 0;
+
+  while(index != -1) {
+    index = data.indexOf("\n");
+    if (index != -1) {
+      menuBox(data.substring(0, index), rank, rank == position);
+    } else {
+      menuBox(data, rank, rank == position);
+    }
+    data = data.substring(index+1, data.length());
+    rank++;
+  }
 }
--- libraries/VCom/VComScreen.h
+++ libraries/VCom/VComScreen.h
@@ -24,21 +24,46 @@
 #include "Arduino.h"
 #include "TFT_eSPI.h"
 
+#define SCREEN_BACK_WORD "< back"
+
 class VComScreen
 {
   public:
 
-    void begin();
-    void menu();
+    void begin(String* menu, int size);
+    void next(int i);
+    void click(std::function<bool(String)> callback);
 
     void clear()                           { _tft.fillScreen(TFT_BLACK); }
     void title(String text, int x, int y)  { _tft.setTextColor(TFT_LIGHTGREY); _tft.drawString(text, x, y, 4); }
     void text(String text, int x, int y)   { _tft.setTextColor(TFT_LIGHTGREY); _tft.drawString(text, x, y, 2); }
-
   
+    void menuBox(String text, int rank, bool isSelected) {
+      int y = rank * 20;
+      if (isSelected) {
+        _tft.setTextColor(TFT_WHITE);
+        _tft.fillRect(0, y, 240, 20, TFT_BLUE);  
+      } else {
+        _tft.setTextColor(TFT_LIGHTGREY);
+      }
+      _tft.drawCentreString(text, 120, y+2, 2);
+    }
+
   private:
 
-    TFT_eSPI _tft;  
+    TFT_eSPI _tft; 
+    String*  _menu;
+    int      _size;
+    int      _position = 0;
+    String   _word = "";
+
+    String   _enumMenu();
+    String   _matchMenu(String command);
+    String   _addData(String data, String command);
+    int      _countData(String data);
+    String   _getData(String data, int position);
+    void     _iterateData(String data, int position);
+    
 };
 
 #endif
